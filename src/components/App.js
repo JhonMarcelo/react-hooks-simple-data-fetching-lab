3e022c478835eeba02d0252e5b950764dc7a8b8d
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-21 13:11:21 -0400

    Complete the lab  by using the fox API and not the dog one as its not working

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://randomfox.ca/floof/")
+      .then((r) => r.json())
+      .then((data) => setImage(data.image));
+  }, []);
+  if (!image) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
